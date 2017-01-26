8cc937d21b4eee5ab2b13053af660682d0c5c2a3
glenn packer <glenn.packer@yahoo.co.uk>
2017-01-26 09:52:16 +0000

    recruit static

---
+++ Recruit.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recruit.Web.MvcApplication" Language="C#" %>
---
+++ Recruit.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recruit.Web.Startup))]
+namespace Recruit.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
